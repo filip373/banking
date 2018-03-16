639bdace72a94967d6fcd85a91d41843f4b6a623
Filip Soszyński <filip.soszynski@netguru.co>
2018-03-16 15:23:49 +0100

    Printing money with better format

--- app/services/print/euro.rb
+++ app/services/print/euro.rb
@@ -7,7 +7,17 @@ module Print
     end
 
     def call
-      "#{@amount} EUR"
+      "#{print_amount} EUR"
+    end
+
+    private
+
+    def print_amount
+      printed = @amount.to_s
+      dots_indexes = (-printed.length...-3).select { |n| (n % 3).zero? }
+      dots_indexes.each { |i| printed.insert i, '.' }
+      printed.insert(-3, ',')
+      printed
     end
   end
 end
